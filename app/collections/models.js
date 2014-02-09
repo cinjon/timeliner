f354d9932fc593f57346b03cb2fae8683719a8a2
cinjon <cinjon.resnick@gmail.com>
2014-02-09 17:59:29 -0500

    models: changed structure, but not implementation

--- app/collections/models.js
+++ app/collections/models.js
@@ -1,9 +1,9 @@
 //All editing permissions go through the User model that comes in Meteor
 
-// Shows - {name:String, home_page:url, description:String, s3:Url, created_at:Date, route:String} //route looks like show name with spaces replaced by -, e.g. Joe-Rogan-Experience
+// Shows - {name:String, home_page:url, description:String, s3:Url, created_at:Date, show_route:String} //route is how name with spaces replaced by -, e.g. Joe-Rogan-Experience
 Shows = new Meteor.Collection('shows');
 
-// Episode - {name:String, home_url:String, show_id:String, number:Number, edited:Boolean, seconds:Number, s3:Url, created_at:Date, updated_at:Date, home_notes:String}
+// Episode - {name:String, home_url:String, show_id:String, show_route:String, number:Number, edited:Boolean, seconds:Number, s3:Url, created_at:Date, updated_at:Date, home_notes:String}
 Episodes = new Meteor.Collection('episodes');
 
 // Clips - {start:Number, end:Number, notes:String, episode_id:String, editor_id:String, previous_clip_id:String, next_clip_id:String, created_at:Date, updated_at:Date}
