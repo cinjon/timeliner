d0b0d0a4d94c9e1bf0b97bf7fd57788420aee787
cinjon <cinjon.resnick@gmail.com>
2014-02-09 18:00:30 -0500

    publishing + router, changes to reflect editor and queue

--- app/client/js/router.js
+++ app/client/js/router.js
@@ -1,26 +1,54 @@
+//nextmarket_id
+//commonsense_id
+//nextmarket_66_id
+//commonsense_257_id
+
+Router.configure({
+    layoutTemplate:'base',
+});
+
 Router.map(function() {
     this.route('home', {
         path:'/',
-        layoutTemplate:'base'
     });
     this.route('testEditor', {
         path:'/testEditor',
-        layoutTemplate:'base',
+    });
+    this.route('queue', {
+        path:'/queue',
+        waitOn: function() {
+            return [Meteor.subscribe('unedited_episodes'), Meteor.subscribe('shows_with_unedited_episodes')];
+        },
+        before: function() {
+            var ret = [];
+            Shows.find().forEach(function(show) {
+                var episodes = Episodes.find({show_id:show._id, edited:false}).fetch();
+                if (episodes.length > 0) {
+                    ret.push({'show':show,
+                              'episodes':episodes
+                             });
+                }
+            });
+            Session.set('queue_data', ret);
+        }
     });
     this.route('editor', {
-        path:'/editor/:show_route/:episode',
+        path:'/editor/:show_route/:number',
         layoutTemplate:'base',
-//         waitOn: function() {
-//         },
-//         data: function() {
-//         }
+        waitOn: function() {
+            var show_route = this.params.show_route;
+            var episode = this.params.number;
+            return [Meteor.subscribe('show_from_route', show_route), Meteor.subscribe('episode_from_show', show_route, episode)];
+        },
+        data: function() {
+            var show = Shows.findOne({_id:this.params.show_route});
+            var episode = Episodes.findOne({show_route:this.params.show_route,
+                                            number:this.params.number});
+            return {show:show, episode:episode};
+        }
     });
     this.route('viewer', {
         path:'/view/',
         layoutTemplate:'base',
-        waitOn: function() {
-        },
-        data: function() {
-        }
     });
 });
---
+++ app/server/publish.js
@@ -0,0 +1,21 @@
+Meteor.publish('show_from_route', function(show_route) {
+    return Shows.find({show_route:show_route});
+});
+
+Meteor.publish('episode_from_show', function(show_route, episode) {
+    return Episodes.find({show_route:show_route, episode:episode});
+});
+
+Meteor.publish('unedited_episodes', function() {
+    return Episodes.find({edited:false});
+});
+
+Meteor.publish('shows_with_unedited_episodes', function() {
+    var show_ids = [];
+    Episodes.find({edited:false}).forEach(function(episode) {
+        show_ids.push(episode.show_id);
+    });
+    return Shows.find({_id:{$in:show_ids}});
+});
+
+
