502a50cb200d87a4624c796745a1bc48730f160d
cinjon <cinjon.resnick@gmail.com>
2014-02-09 22:17:44 -0500

    editor: html + display, next up is the player

--- app/client/js/editor.js
+++ app/client/js/editor.js
@@ -1,3 +1,27 @@
 Template.editor.rendered = function() {
 
 }
+
+Template.editor.episode_name = function() {
+    if (Session.get('episode')) {
+        return Session.get('episode').name;
+    }
+}
+
+Template.editor.episode_number = function() {
+    if (Session.get('episode')) {
+        return Session.get('episode').number;
+    }
+}
+
+Template.editor.url = function() {
+    if (Session.get('episode')) {
+        return Session.get('episode').s3;
+    }
+}
+
+Template.editor.show_name = function() {
+    if (Session.get('show')) {
+        return Session.get('show').name;
+    }
+}
