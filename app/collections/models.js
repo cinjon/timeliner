899d1a7f73671905b43fbccb452de1c1dac63166
Matt Tanase <matthewt@gmail.com>
2014-02-11 00:41:07 +0000

    added users.js to client and server, cleaned model comments

--- app/collections/models.js
+++ app/collections/models.js
@@ -1,12 +1,15 @@
 //All editing permissions go through the User model that comes in Meteor
 
-// Shows - {name:String, home_page:url, description:String, s3:Url, created_at:Date, show_route:String} //route is how name with spaces replaced by -, e.g. Joe-Rogan-Experience
+// Shows - {name:String, home_page:url, description:String, s3:Url, created_at:Date, show_route:String}
+//route is how name with spaces replaced by -, e.g. Joe-Rogan-Experience
 Shows = new Meteor.Collection('shows');
 
-// Episode - {name:String, home_url:String, show_id:String, show_route:String, number:Number, edited:Boolean, seconds:Number, s3:Url, created_at:Date, updated_at:Date, home_notes:String}
+// Episode - {name:String, home_url:String, show_id:String, show_route:String, number:Number,
+// edited:Boolean, seconds:Number, s3:Url, created_at:Date, updated_at:Date, home_notes:String}
 Episodes = new Meteor.Collection('episodes');
 
-// Clips - {start:Number, end:Number, notes:String, episode_id:String, editor_id:String, previous_clip_id:String, next_clip_id:String, created_at:Date, updated_at:Date}
+// Clips - {start:Number, end:Number, notes:String, episode_id:String, editor_id:String,
+// previous_clip_id:String, next_clip_id:String, created_at:Date, updated_at:Date}
 Clips = new Meteor.Collection('clips');
 
 // Links - {url:String, clip_id:String, episode_id:String, text:String, created_at:String}
