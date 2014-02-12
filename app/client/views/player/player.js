497ae31d94c262a19a0d54e3792ea675daf021bf
cinjon <cinjon.resnick@gmail.com>
2014-02-12 15:26:43 -0500

    player: move to its own directory

--- app/client/views/player/player.js
+++ app/client/views/player/player.js
@@ -1,5 +1,4 @@
-Template.player.rendered = function() {
-
+Template.editor_player.rendered = function() {
     videojs("#player", {"controls":true, "preload":"auto", "autoplay":false}, function(){});
     $('.vjs-big-play-button').css("margin-top", "-1.33em"); //to fix the play button, may not actually be consistent
 
@@ -38,9 +37,17 @@ Template.player.rendered = function() {
     });
 }
 
+Template.view_player.rendered = function() {
+    videojs("#player", {"controls":true, "preload":"auto", "autoplay":false}, function(){});
+    $('.vjs-big-play-button').css("margin-top", "-1.33em"); //to fix the play button, may not actually be consistent
+}
 
-Template.player.destroyed = function() {
+Template.editor_player.destroyed = function() {
     videojs("#player").dispose();
     Meteor.Keybindings.remove(['shift+space', 'shift+←', 'shift+alt+←',
         'shift+→', 'shift+alt+→', 'shift+↑', 'shift+↓' ]);
+}
+
+Template.view_player.destroyed = function() {
+    videojs("#player").dispose();
 }
