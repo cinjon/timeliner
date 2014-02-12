02d3780b160dfbdc728569120a6a00fe6cd328c2
cinjon <cinjon.resnick@gmail.com>
2014-02-12 18:39:20 -0500

    publications: the editors

--- app/server/publications.js
+++ app/server/publications.js
@@ -14,6 +14,16 @@ Meteor.publish('unedited_episodes', function() {
     return Episodes.find({edited:false});
 });
 
+Meteor.publish('editors', function(show_route, number) {
+    //TODO: after making user creation hooks, limit this to username for editors
+    var user_ids = [];
+    Clips.find({show_route:show_route, number:number}).forEach(function(clip) {
+        user_ids.push(clip.editor_id);
+    });
+
+    return Meteor.users.find({_id:{$in:user_ids}}, {"emails.address":1, _id:0});
+});
+
 Meteor.publish('shows_with_unedited_episodes', function() {
     var show_ids = [];
     Episodes.find({edited:false}).forEach(function(episode) {
