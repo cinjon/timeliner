739beb612c6045fd4d84dcdc84eda1e24bbac9fb
cinjon <cinjon.resnick@gmail.com>
2014-02-13 10:45:49 -0500

    client include: global claim functions

--- app/client/lib/include.js
+++ app/client/lib/include.js
@@ -1,3 +1,11 @@
-global_has_role = function(roles) {
+client_global_has_role = function(roles) {
   return Roles.userIsInRole(Meteor.userId(), roles);
+}
+
+client_global_is_claimer = function(claimer_id) {
+    return claimer_id == Meteor.userId();
+}
+
+client_global_unclaimed = function(claimer_id) {
+    return claimer_id == null;
 }
