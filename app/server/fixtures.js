6d02b47ccc984ef465865adc42254cb882072ae0
cinjon <cinjon.resnick@gmail.com>
2014-02-13 23:20:30 -0500

    fixtures: made it legit for production + killed admin.js's roles business

--- app/server/fixtures.js
+++ app/server/fixtures.js
@@ -3,11 +3,19 @@ Meteor.startup(function() {
   if (Shows.find().count() === 0) {
     var timestamp = (new Date()).getTime();
 
-    var user_id = Accounts.createUser({
-      email:'fake@user.com',
-      password:'fakeuserpasswordatmeteor',
-      username:'Fred Editor'
+    var cinjon_id = Accounts.createUser({
+      email:'cinjon.resnick@gmail.com',
+      password:'sharpsharksshank',
+      username:'cinjon'
     });
+    Roles.addUsersToRoles(cinjon_id, ['admin', 'editor', 'user-admin']);
+    var matt_id = Accounts.createUser({
+      email:'matthewt@gmail.com',
+      password:'greenfishpray',
+      username:'matt'
+    });
+    Roles.addUsersToRoles(matt_id, ['admin', 'editor', 'user-admin']);
+
 
     var nextmarket_id = Shows.insert({
       name: 'NextMarket',
@@ -23,6 +31,141 @@ Meteor.startup(function() {
       created_at: timestamp,
       description: "Common Sense with Dan Carlin is an independent look at politics and current events from popular New Media personality Dan Carlin."
     });
+    var fatman_id = Shows.insert({
+      name:'Fat Burning Man',
+      home_url:'http://www.fatburningman.com/category/podcasts/',
+      show_route:'Fat-Burning-Man',
+      created_at:timestamp,
+      description:null,
+    });
+    var primal_id = Shows.insert({
+      name:'Primal Blueprint Podcast',
+      home_url:'http://blog.primalblueprint.com/',
+      show_route:'Primal-Blueprint-Podcast',
+      created_at:timestamp,
+      description:"The Primal Blueprint is about helping you discover how amazingly simple and fun lifelong wellness can be."
+    });
+    var randomshow_id = Shows.insert({
+      name:'The Random Show',
+      home_url:'http://www.fourhourworkweek.com/blog/',
+      show_route:'The-Random-Show',
+      created_at:timestamp,
+      description:null
+    });
+
+    var gottfried_id = Episodes.insert({
+      name: 'Dr. Sara Gottfried',
+      home_url: 'http://www.fatburningman.com/dr-sara-gottfried-hormones-for-men/',
+      created_at: timestamp,
+      updated_at: null,
+      show_id: fatman_id,
+      edited: false,
+      claimed_id: null,
+      s3: 'http://s3timeliner.s3.amazonaws.com/fat-burning-man/82.mp3',
+      home_notes: null,
+      seconds: null,
+      number: 82,
+      show_route: 'Fat-Burning-Man'
+    });
+    var supplementation = Episodes.insert({
+      name: 'Supplementation',
+      home_url: 'http://blog.primalblueprint.com/episode-5-supplementation-with-mark-sisson/',
+      created_at: timestamp,
+      updated_at: null,
+      show_id: primal_id,
+      edited: false,
+      claimed_id: null,
+      s3: 'http://s3timeliner.s3.amazonaws.com/primal-blueprint-podcast/4.mp3',
+      home_notes: null,
+      seconds: null,
+      number: 4,
+      show_route: 'Primal-Blueprint-Podcast'
+    });
+    var tenlaws = Episodes.insert({
+      name: 'The 10 Laws',
+      home_url: 'http://blog.primalblueprint.com/episode-4-the-10-laws-with-mark-sisson/',
+      created_at: timestamp,
+      updated_at: null,
+      show_id: primal_id,
+      edited: false,
+      claimed_id: null,
+      s3: 'http://s3timeliner.s3.amazonaws.com/primal-blueprint-podcast/5.mp3',
+      home_notes: null,
+      seconds: null,
+      number: 5,
+      show_route: 'Primal-Blueprint-Podcast'
+    });
+    var random_18 = Episodes.insert({
+      name: 'Episode 18',
+      home_url: 'http://www.fourhourworkweek.com/blog/2012/09/17/the-random-show-episode-18-start-ups-restaurants-marriage-and-sexual-performance/',
+      created_at: timestamp,
+      updated_at: null,
+      show_id: randomshow_id,
+      edited: false,
+      claimed_id: null,
+      s3: 'http://s3timeliner.s3.amazonaws.com/the-random-show/18.mp3',
+      home_notes: null,
+      seconds: null,
+      number: 18,
+      show_route: 'The-Random-Show'
+    });
+    var random_20 = Episodes.insert({
+      name: 'Episode 19',
+      home_url: null,
+      created_at: timestamp,
+      updated_at: null,
+      show_id: randomshow_id,
+      edited: false,
+      claimed_id: null,
+      s3: 'http://s3timeliner.s3.amazonaws.com/the-random-show/19.mp3',
+      home_notes: null,
+      seconds: null,
+      number: 19,
+      show_route: 'The-Random-Show'
+    });
+    var random_20 = Episodes.insert({
+      name: 'Episode 20',
+      home_url: 'http://www.fourhourworkweek.com/blog/2013/01/17/random-show-episode-20-dog-aerobics-start-ups-meditation-comic-books-and-new-years-resolutions/',
+      created_at: timestamp,
+      updated_at: null,
+      show_id: randomshow_id,
+      edited: false,
+      claimed_id: null,
+      s3: 'http://s3timeliner.s3.amazonaws.com/the-random-show/20.mp3',
+      home_notes: null,
+      seconds: null,
+      number: 20,
+      show_route: 'The-Random-Show'
+    });
+    var random_21 = Episodes.insert({
+      name: 'Episode 21',
+      home_url: 'http://www.fourhourworkweek.com/blog/2013/06/05/the-random-show-episode-21-smart-drugs-bitcoin-apps-fish-taint-and-more/',
+      created_at: timestamp,
+      updated_at: null,
+      show_id: randomshow_id,
+      edited: false,
+      claimed_id: null,
+      s3: 'http://s3timeliner.s3.amazonaws.com/the-random-show/21.mp3',
+      home_notes: null,
+      seconds: null,
+      number: 21,
+      show_route: 'The-Random-Show'
+    });
+    var random_22 = Episodes.insert({
+      name: 'Episode 22',
+      home_url: 'http://www.fourhourworkweek.com/blog/2013/08/12/the-random-show-episode-22-home-defense-start-ups-raccoon-throwing-books-and-mail-order-urine/',
+      created_at: timestamp,
+      updated_at: null,
+      show_id: randomshow_id,
+      edited: false,
+      claimed_id: null,
+      s3: 'http://s3timeliner.s3.amazonaws.com/the-random-show/22.mp3',
+      home_notes: null,
+      seconds: null,
+      number: 22,
+      show_route: 'The-Random-Show'
+    });
+
 
     var nextmarket_66_id = Episodes.insert({
       name: 'Robert Scoble',
@@ -32,98 +175,24 @@ Meteor.startup(function() {
       show_id: nextmarket_id,
       edited: false,
       claimed_id: null,
-      s3: 'http://s3timeliner.s3.amazonaws.com/nextmarket_podcast/NextMarket66.mp3',
-      home_notes: 'Slightly too lazy to incorporate',
+      s3: 'http://s3timeliner.s3.amazonaws.com/nextmarket-podcast/66.mp3',
+      home_notes: null,
       seconds: 2962,
       number: 66,
       show_route: 'NextMarket'
     });
     var commonsense_257_id = Episodes.insert({
       name: 'Monopolizing the Democracy',
-      home_url: 'http://podbay.fm/show/155974141/e/1373435424',
+      home_url: 'http://www.dancarlin.com//disp.php/csarchive/Show-257---Monopolizing-the-Democracy/Egypt-Coup-David%20Brooks',
       created_at: timestamp,
       updated_at: null,
       show_id: commonsense_id,
       edited: false,
       claimed_id: null,
-      s3: 'http://s3timeliner.s3.amazonaws.com/common_sense_with_dan_carlin/cswdcc257.mp3',
+      s3: 'http://s3timeliner.s3.amazonaws.com/common-sense-with-dan-carlin/257.mp3',
       seconds: 2538,
       number: 257,
       show_route: 'Common-Sense-With-Dan-Carlin'
-    });
-
-    var fake_first_nextmarket_clip_id = Clips.insert({
-      start: 10,
-      end: 110,
-      notes: "I have two rows in two containers, but I can't figure out how to reduce the bottom and top margin/padding to bring the content closer.",
-      episode_id: nextmarket_66_id,
-      editor_id: user_id,
-      previous_clip_id: null,
-      next_clip_id: null,
-      created_at: timestamp,
-      updated_at: timestamp
-    });
-    var fake_second_nextmarket_clip_id = Clips.insert({
-      start: 300,
-      end: 603,
-      notes: "The future of layout in CSS, Flexbox is the latest CSS spec designed to solve common layout problems such as vertical centering.",
-      episode_id: nextmarket_66_id,
-      editor_id: user_id,
-      previous_clip_id: fake_first_nextmarket_clip_id,
-      next_clip_id: null,
-      created_at: timestamp,
-      updated_at: timestamp
-    });
-    Clips.update({
-      id: fake_first_nextmarket_clip_id
-    }, {
-      $set: {
-        next_clip_id: fake_second_nextmarket_clip_id
-      }
-    });
-
-    var first_link_id = Links.insert({
-      url: 'http://bootply.com/bootstrap-3-migration-guide',
-      text: "What's New in Bootstrap 3",
-      created_at: timestamp
-    })
-    var second_link_id = Links.insert({
-      url: 'http://wrapbootstrap.com/preview/WB022B0X6',
-      text: "Von - Minimalist Blog Theme",
-      created_at: timestamp
-    });
-    var third_link_id = Links.insert({
-      url: 'http://www.bizjournals.com/denver/stories/2009/04/27/daily24.html?page=all',
-      text: 'John Malone Talks',
-      created_at: timestamp
-    });
-
-    Clips.update({
-      _id: fake_first_nextmarket_clip_id
-    }, {
-      $push: {
-        links: {
-          $each: [second_link_id, third_link_id]
-        }
-      }
-    });
-    Clips.update({
-      _id: fake_second_nextmarket_clip_id
-    }, {
-      $push: {
-        links: {
-          $each: [first_link_id, third_link_id]
-        }
-      }
-    });
-    Episodes.update({
-      _id: nextmarket_66_id
-    }, {
-      $push: {
-        links: {
-          $each: [first_link_id, second_link_id, third_link_id]
-        }
-      }
     });
   }
 });
