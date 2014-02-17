53857bef5de1c64d51c404618d8c7dea593aaa95
Matt Tanase <matthewt@gmail.com>
2014-02-17 20:06:48 +0000

    hoping the keypress double fire bug is fixed with e.preventDefault()

--- client/views/player/player.js
+++ client/views/player/player.js
@@ -6,6 +6,8 @@ Template.editor_player.rendered = function() {
   // theory: rerendered player was executing each shortcut multiple times
   Meteor.Keybindings.removeAll();
 
+  // preventDefault() appears to fix a jquery bug that has been documented but
+  // should have been fixed. Testing and it seems to work so I'll stick with it.
   Meteor.Keybindings.add({
     'ctrl+space': function() {
       if (videojs("#player").paused()) {
@@ -13,34 +15,43 @@ Template.editor_player.rendered = function() {
       } else {
         videojs("#player").pause();
       }
+      e.preventDefault();
     },
     'ctrl+,': function () {
       time = videojs("#player").currentTime();
       videojs("#player").currentTime(time - 5);
+      e.preventDefault();
     },
     'ctrl+.': function () {
       time = videojs("#player").currentTime();
       videojs("#player").currentTime(time + 5);
+      e.preventDefault();
     },
     'ctrl+1': function () {
       time = videojs("#player").currentTime();
       global_record_time("start_time");
+      e.preventDefault();
     },
     'ctrl+2': function () {
       time = videojs("#player").currentTime();
       global_record_time("end_time");
+      e.preventDefault();
     },
     'ctrl+3': function () {
       $('#notes').focus();
+      e.preventDefault();
     },
     'ctrl+4': function () {
       $('#link_url').focus();
+      e.preventDefault();
     },
     'ctrl+5': function () {
       $('#link_text').focus();
+      e.preventDefault();
     },
     'ctrl+6': function () {
       $('#add_link').click();
+      e.preventDefault();
     }
   });
 };
@@ -50,8 +61,8 @@ Template.view_player.rendered = function() {
 };
 
 Template.editor_player.destroyed = function() {
-    dispose_video();
     Meteor.Keybindings.removeAll();
+    dispose_video();
 };
 
 Template.view_player.destroyed = function() {
