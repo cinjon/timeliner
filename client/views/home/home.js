b85393bf46799865ff85157a297ec1cd0509337c
cinjon <cinjon.resnick@gmail.com>
2014-02-22 16:14:34 -0800

    home: made a temporary landing page that highlights the editor flyer

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,11 +1,16 @@
+open_for_business = false;
+
 Template.home.helpers({
-    shows: function() { //should only return what server decides are top shows
-        return Shows.find();
-    },
-    edited_episodes: function() {
-        return Episodes.find({show_id:this._id, edited:true});
-    },
-    unedited_episodes: function() {
-        return Episodes.find({show_id:this._id, edited:false});
-    }
+  shows: function() { //should only return what server decides are top shows
+    return Shows.find();
+  },
+  edited_episodes: function() {
+    return Episodes.find({show_id:this._id, edited:true});
+  },
+  unedited_episodes: function() {
+    return Episodes.find({show_id:this._id, edited:false});
+  },
+  open_for_business: function() {
+    return open_for_business;
+  }
 });
